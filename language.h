60a0a601367851a0368538bf2a5632c09e272519
hhzb123 <13205953135@163.com>
2017-04-23 23:22:01 +0800

    Create language.h

---
+++ language.h
@@ -0,0 +1,32 @@
+#include<stdio.h>
+void Chinese(int a)
+{    if(a==0) printf("萩補秘籾朕方楚:");
+	if(a==1) printf("屎鳩基宛頁?");
+	if(a==2) printf("屎鳩:"); 
+	if(a==3) printf("危列:");
+} 
+void English(int a)
+{   if(a==0) printf("Please enter the number of questions:") ;
+	if(a==1) printf("The correct answer is:");
+	if(a==2) printf("right:");
+	if(a==3) printf("wrong:");
+}
+void German(int a)
+{  if(a==0) printf("Bitte geben Sie die Anzahl der Elemente:"); 
+	if(a==1) printf("Die richtige Antwort ist:");
+	if(a==2)printf("richtig:");
+	if(a==3) printf("Fehler:");
+}
+void French(int a)
+{if(a==0) printf("S'il vous pla?t entrez le nombre de sujets:") ;
+if(a==1) printf("La bonne r└ponse est");
+if(a==2) printf("correct:");
+if(a==3) printf("erreur:");
+}
+void Japanese(int a)
+{ if(a==0) printf("?朕の桑催を秘薦してください:");
+	if(a==1) printf("屎しい基えは肝のとおりです");
+	if(a==2) printf("屎しいです:");
+	if(a==3) printf("エラ`:");
+}
+
